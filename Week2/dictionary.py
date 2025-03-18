de34d8451ea831650a12036dea6a18d981b1c9ee
Hillan007 <sc21232332023@mut.ac.ke>
2025-03-18 13:46:48 +0300

    now or never

---
+++ Week2/dictionary.py
@@ -0,0 +1,11 @@
+# Create an empty dictionary to store the person's information
+person_info = {}
+
+# Prompt the user for input and store the information in the dictionary
+person_info['name'] = input("Enter your name: ")
+person_info['age'] = input("Enter your age: ")
+person_info['favorite_color'] = input("Enter your favorite color: ")
+
+# Print the dictionary to the console
+print("\nHere is the information you provided:")
+print(person_info)
---
+++ Week2/tuple.py
@@ -0,0 +1,6 @@
+# Creating a tuple of book names
+favorite_books = ("To Kill a Mockingbird", "1984", "The Great Gatsby", "Pride and Prejudice", "The Catcher in the Rye")
+
+# Using a for loop to print each book name
+for book in favorite_books:
+    print(book)
