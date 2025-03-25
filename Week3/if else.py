e1e0ba9256667ac69b998d7275433da28b9c958b
Hillan007 <sc21232332023@mut.ac.ke>
2025-03-25 12:52:36 +0300

    done and dusted

---
+++ Week3/if else.py
@@ -0,0 +1,7 @@
+# Example of an if-else statement
+temperature = 26
+
+if temperature > 25:
+    print("It's a hot day!")
+else:
+    print("It's a cool day!")
---
+++ Week3/if.py
@@ -0,0 +1,5 @@
+# Checking if a number is positive
+number = 10
+
+if number > 0:
+    print("The number is positive.")
